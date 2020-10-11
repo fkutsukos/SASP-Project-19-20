66d0b4dd0973f766dca1806f8fe344c5c4d18a9b
Fotis Kutsukos <fotis.kutsukos@gmail.com>
2020-10-11 19:52:22 +0200

    created src folder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import roomGeometry
+#import roomGeometry
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
