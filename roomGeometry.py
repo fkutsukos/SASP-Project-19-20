42c0172e7858350b9bb6bde72ca461a7d9a9644f
Fotis Kutsukos <fotis.kutsukos@gmail.com>
2020-10-11 19:29:13 +0200

    Room with M=5 microphones

--- roomGeometry.py
+++ roomGeometry.py
@@ -2,23 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyroomacoustics as pra
 
+# The desired reverberation time and dimensions of the room
+rt60 = 1.0  # seconds
+length = 15.0  # meters
+width = 10.0
+height = 3.0
+room_dim = [length, width, height]
 
-# Create a 2D room from the corners of a polygon
-floor = np.array([[0, 0, 10, 10],   # x−coordinates
-                  [0, 10, 10, 0]])  # y−coordinates
-room = pra.Room.from_corners(floor, fs=16000, max_order=12, absorption=0.1)
-# Lift the room in 3D space
-room.extrude(2.4)
+# We invert Sabine's formula to obtain the parameters for the ISM simulator
+e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
+
+# Create a shoebox room
+room = pra.ShoeBox(
+    room_dim, fs=16000, materials=pra.Material(e_absorption), max_order=max_order
+)
+
 # Add a sound source
 room.add_source([1.5, 1.2, 1.6])
-# Place two microphones in the room
-R = np.array([[3., 4.2], [2.25, 2.1], [1.4, 1.4]])
+
+# Place a linear 2D array in the room
+center = np.array([np.floor(length / 2), np.floor(width / 2), np.floor(height / 2)])
+d = 1  # distance between array
+x1 = center[0]
+x2 = x1 + d
+x3 = x2 + d
+x4 = x1 - d
+x5 = x4 - d
+y = center[1]
+z = center[2]
+R = np.array([[x1, x2, x3, x4, x5], [y, y, y, y, y], [z, z, z, z, z]])
 bf = pra.MicrophoneArray(R, room.fs)
 room.add_microphone_array(bf)
-# Run image source model and show room and 2nd order images
-room.image_source_model()
-room.plot(img_order=3, aspect='equal')
 
-fig = plt.figure()
+# Run image source model and show room and 2nd order images
+
+room.image_source_model()
+fig = plt.figure("Rirs")
 room.plot_rir()
+room.plot(img_order=1, aspect='equal')
 plt.show()
