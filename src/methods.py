89730defb9a5048730f404798bcfe4a0942a501a
olivieromassi <oliviero.massi@mail.polimi.it>
2020-10-30 19:10:02 +0100

    Implemented echo_labeling method: need to check if the test over the rank is sufficient to discriminate an EDM matrix or if it is necessary to resort to some kind of Multi Dimensional Scaling

--- src/methods.py
+++ src/methods.py
@@ -17,7 +17,7 @@ def build_room(dimensions, source, mic_array, rt60, fs):
     e_absorption, max_order = pra.inverse_sabine(rt60, dimensions)
 
     # Building a 'Shoebox' room with the provided dimensions
-    room = pra.ShoeBox(p=dimensions, fs=fs, absorption=e_absorption, max_order= max_order)
+    room = pra.ShoeBox(p=dimensions, fs=fs, absorption=e_absorption, max_order=max_order)
 
     # Place the Microphone Array and the Sound Source inside the room
     mics = pra.MicrophoneArray(mic_array, fs)
@@ -54,23 +54,51 @@ def build_edm(mic_locations):
     return D
 
 
-def echoLabeling(EDM,peaksArray):
-    peaks = np.array([[20, 25, 30, 35],
-                          [25, 30, 36, 40]])
-    c = 340 # speed of sound
-    Fs = 16000 # sampling freq
-    peaks *= c/Fs
-    #for each row of peaks
-        #for each column of peaks
-            #rankTest
-            #if rankTest == True
-             #   store combination of distances/image sources
-             #   array indexes of echoes 1st image source [20 , 30] , 2nd [30 , 36] , 3rd [35, 40] 4th ...
+def echo_labeling(edm, mic_peaks, fs, c=343.0):
+    """
+    This method evaluates if the matrix Daug, generated adding a row and
+    a column made of the possible echo combinations squared to the
+    EDM matrix built over the microphone distances is still an
+    Euclidean Distance Matrix;
+    :param edm: Euclidean Distance Matrix built using the distances
+    between the different microphones.
+    :param mic_peaks: list of arrays containing the indexes of the
+    peaks found in the RIR captured by each microphone.
+    :param fs: sampling frequency used in the RIRs measure.
+    :param c: speed of sound (set to the default value of 343 m/s).
+    :return: a list containing all the echo combinations that satisfy
+    the EDM property
+    """
+    # Converting the RIR peak location indexes into space distances
+    mic_peaks *= c/fs
 
-             #   break;
+    # Building a grid of each possible echo combination
+    echoes_comb = np.array(np.meshgrid(*mic_peaks)).T.reshape(-1, len(mic_peaks))
 
+    # Getting the input EDM dimensions and setting Daug dimensions
+    dim = edm.shape[0]
+    dim += 1
 
-    return peaksToImageSourceArray
+    # Instantiating the Daug matrix
+    d_aug = np.zeros(shape=(dim, dim))
+
+    # Start building Daug inserting the initial EDM
+    d_aug[0:dim-1, 0:dim-1] = edm
+    # Instantiate the list of matching echoes
+    echoes_match = []
+
+    # Echo labeling process
+    for echo in echoes_comb:
+        # Adding the echo combination squared
+        d_aug[0:-1, -1] = echo ** 2
+        d_aug[-1, 0:-1] = echo ** 2
+        print(d_aug)
+        # In a k dimensional space, the rank cannot be greater than k + 2
+        if np.linalg.matrix_rank(d_aug) < 6:
+            print(np.linalg.matrix_rank(d_aug))
+            echoes_match.append(echo)
+    print(echoes_match)
+    return echoes_match
 
 
 def trilateration(sortedEchoestoImageSources):
