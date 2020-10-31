f4df8c8b85f651f3ef108b630536df431a403077
olivieromassi <oliviero.massi@mail.polimi.it>
2020-10-31 16:59:00 +0100

    Added some testing using the data from the Murphy-Hereman Trilateration paper

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import roomGeometry
 import methods
+import numpy as np
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,9 +13,12 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-     roomGeometry
-
-    #for mic in rirs.shape[0]:
-    #     methods(rirs)
+    # Testing the Trilateration method with the data contained in the paper Murphy-Hereman Trilateration
+    source =methods.trilateration(np.array([[475060, 481500, 482230, 478050, 471430, 468720, 467400, 468730],
+                                            [1096300, 1094900, 1088430, 1087810, 1088580, 1091240, 1093980, 1097340],
+                                            [4670, 4694, 4831, 4775, 4752, 4803, 4705, 4747]]),
+                                  np.array([5942.607, 2426.635, 5094.254, 5549.874, 9645.353, 11419.870, 12639.330, 12078.820]))
+    print(source)
+    print_hi("SASP")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
