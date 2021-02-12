357ac9e24b7f6af7bc88e02ba63004a4054db3ab
Fotis Kutsukos <fotis.kutsukos@gmail.com>
2021-02-12 18:38:15 +0100

    updated main.py to receive user input

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import methods, peaking_test
+import methods, peaking_test, echo_test
 import numpy as np
 import matplotlib.pyplot as plt
+import load_save as ls
+
 
 
 def print_hi(name):
@@ -23,42 +25,89 @@ if __name__ == '__main__':
                                              12078.820]))
     print(source)
     """
-    # Testing the build room using input from json file
-    room_data = methods.input_data(file_dir="../input", file_name="room.json")
-    rt60 = float(room_data['rt60'])
-    dimensions = list(map(float, room_data['dimensions']))
-    mic_array = [list(map(float, lst)) for lst in room_data['mic_array']]
-    source = list(map(float, room_data['source']))
-    fs = int(room_data['fs'])
+    compute = False
+    restore = False
 
-    room, global_delay = methods.build_room(dimensions, source, mic_array, rt60, fs)
-    edm = methods.build_edm(np.array(mic_array))
+    while True:
+        print('Type \'C\' for computing the echoes OR \'R\' for restoring them')
+        try:
+            cmd = input('> : ')
 
-    '''
-    Code to implement spline interpolation.
-    mic_rirs = []
+        except KeyboardInterrupt:
+            print('Exiting')
+            break
 
-    for mic in range(len(room.rir)):
-        mic_rirs.append(methods.spline_interpolation(room.rir[mic][0], 100))
-    '''
-    peaks = peaking_test.find_echoes(room.rir, global_delay=global_delay, n=5).astype(int)
+        try:
+            # Testing the build room using input from json file
+            configuration_file = "room.json"
+            csv_file_name = configuration_file.split('.')[0] + '.csv'
 
-    # Trying to locate the source considering only one peak per RIR
-    # peaks = methods.peak_picking(room.rir, 3)
-    echoes = methods.echo_labeling(edm, np.array(peaks, dtype=float), 2, fs, global_delay)
+            room_data = methods.input_data(file_dir="../input", file_name=configuration_file)
+            rt60 = float(room_data['rt60'])
+            dimensions = list(map(float, room_data['dimensions']))
+            mic_array = [list(map(float, lst)) for lst in room_data['mic_array']]
+            loudspeaker = list(map(float, room_data['source']))
+            fs = int(room_data['fs'])
 
-    virtual_sources = []
-    echoes.sort()
-    # Computing and printing in 3D the virtual sources
-    fig = plt.figure()
-    # ax = plt.axes(projection="3d")
-    ax = plt.axes()
-    for echo in echoes[:10]:
-        # if echo[0] < np.inf:
-        # source = methods.trilaterate_beck(np.array(mic_array).transpose(), echo[1])
-        source = methods.trilateration(np.array(mic_array), echo[1])
-        virtual_sources.append(source)
-        # ax.scatter3D(*source)
-        ax.scatter(*source)
-    plt.show()
+            room, global_delay = methods.build_room(dimensions, loudspeaker, mic_array, rt60, fs)
 
+        except Exception as e:
+            print(e)
+
+        if cmd == 'C':
+            print('Computing the echoes..')
+            try:
+                edm = methods.build_edm(np.array(mic_array))
+                '''
+                Code to implement spline interpolation.
+                mic_rirs = []
+            
+                for mic in range(len(room.rir)):
+                    mic_rirs.append(methods.spline_interpolation(room.rir[mic][0], 100))
+                '''
+                peaks = peaking_test.find_echoes(room.rir, global_delay=global_delay, n=5).astype(int)
+
+                # Trying to locate the source considering only one peak per RIR
+                # peaks = methods.peak_picking(room.rir, 3)
+
+                #echoes = methods.echo_labeling(edm, np.array(peaks, dtype=float), 2, fs, global_delay)
+                echoes = echo_test.echo_sorting(edm, np.array(peaks, dtype=float), 2, 4.0, fs, global_delay)
+
+
+                csv_file_name = configuration_file.split('.')[0] + '.csv'
+                number_of_microphones = np.array(mic_array).shape[1]
+                ls.save_to_csv(echoes, csv_file_name, number_of_microphones)
+                print('echoes are saved to ' + csv_file_name)
+
+            except Exception as e:
+                print(e)
+
+        if cmd == 'R':
+            try:
+                print('echoes are laoded from ' + csv_file_name)
+                echoes = ls.load_from_csv(csv_file_name)
+            except Exception as e:
+                print(e)
+        try:
+            virtual_sources = []
+            echoes.sort()
+            # Computing and printing in 3D the virtual sources
+            fig = plt.figure()
+            # ax = plt.axes(projection="3d")
+            ax = plt.axes()
+            for echo in echoes:
+                # if echo[0] < np.inf:
+                # source = methods.trilaterate_beck(np.array(mic_array).transpose(), echo[1])
+                source = methods.trilateration(np.array(mic_array), echo[1])
+                virtual_sources.append(source)
+                # ax.scatter3D(*source)
+                ax.scatter(*source)
+                print('scattered source no:' , len(virtual_sources) )
+            plt.show()
+            print(0)
+            methods.reconstruct_room(virtual_sources, np.array(loudspeaker), 1e-5)
+
+        except Exception as e:
+            print(e)
+
+        break
